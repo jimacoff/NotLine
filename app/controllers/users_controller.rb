a9953cbc78a968df33b981943a270937fce57639
Tsung-en Hsiao <tsn@cs.nctu.edu.tw>
2018-03-11 16:25:44 +0800

    Finish users#chat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:chat]
   def index
     @users = User.order(name: :asc).page(params[:page]).per(20)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def chat
+    chat = current_user.find_or_create_chat_with(User.find(params[:id]))
+    redirect_to chat_path(chat)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  root to: "chats#index"
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
